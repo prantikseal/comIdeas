d433fb079a6c7e328063285f5c61b711610b2d58
prantikseal <83081442+prantikseal@users.noreply.github.com>
2023-07-16 17:48:19 +0530

    fix: api changes in ideagenerator

--- src/components/IdeaGenerator.jsx
+++ src/components/IdeaGenerator.jsx
@@ -1,9 +1,7 @@
 import "./IdeaGenerator.css";
 import { useState } from "react";
 import axios from "axios";
-// import dotenv from "dotenv";
 
-// dotenv.config();
 
 const IdeaGenerator = () => {
   const [chatGPTResponse, setChatGPTResponse] = useState("Your Ideas will be Listed here :)");
@@ -13,18 +11,19 @@ const IdeaGenerator = () => {
     const ideaPurpose = document.getElementById("idea_purpose").value;
     const companyName = document.getElementById("company_name").value;
     const companyDetails = document.getElementById("company_details").value;
-    // const response = {
-    //     ideaPurpose,
-    //     companyName,
-    //     companyDetails
-    // }
-    const prompt = `Generate 3 Ideas where Idea Purpose: ${ideaPurpose} \n Company Name: ${companyName} \n Company Details: ${companyDetails} to create proof of work profiles and apply for opportunities within the company.`;
+
+    const prompt = `Generate 3 Ideas where Idea Purpose: ${ideaPurpose} \n Company Name: ${companyName} \n Company Details: ${companyDetails} to create proof of work projects and apply for opportunities within the company.`;
     console.log(prompt);
-    // const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
-    const OPENAI_API_KEY= "pk-albal"
+
+    const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+
+    console.log(OPENAI_API_KEY);
+
+
     const response = await axios
       .post(
-        "https://api.pawan.krd/v1/completions",
+
+        "https://chimeragpt.adventblocks.cc/v1/completions",
         {
           model: "text-davinci-003",
           max_tokens: 100,
@@ -37,12 +36,10 @@ const IdeaGenerator = () => {
             "Content-Type": "application/json",
           },
         }
-      )
-      .then((response) => console.log(response))
-      .catch((error) => console.log(error));
-    // console.log(response);
-    // setChatGPTResponse(response.data.choices[0].text);
-    setChatGPTResponse(response.data.error);
+      );
+
+    console.log(response.data.choices[0].text);
+    setChatGPTResponse(response.data.choices[0].text);
   };
   return (
     <section className="ideaGeneratorSection">
